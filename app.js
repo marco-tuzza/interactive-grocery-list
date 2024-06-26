d97ce4fde17d2eab5c5d059fadb8eaffb35c2db0
Marco Tuzza <marcotuzza@hotmail.it>
2024-06-26 20:59:05 +0400

    Added the possibility to click on each element to select the checkbox

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const list = ["apple", "banana", "cherry"];
 const listElement = document.getElementById("list");
 
-list.forEach((item) => {
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.value = item;
-    checkbox.id = item;
-    checkbox.name = item;
+function initializeList() {
+    list.forEach((item) => {
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.value = item;
+        checkbox.id = item;
+        checkbox.name = item;
 
-    const label = document.createElement("label");
-    label.htmlFor = item;
-    label.appendChild(document.createTextNode(item));
+        const label = document.createElement("label");
+        label.htmlFor = item;
+        label.appendChild(document.createTextNode(item));
 
-    const div = document.createElement("div");
-    div.appendChild(checkbox);
-    div.appendChild(label);
-    listElement.appendChild(div);
+        const div = document.createElement("div");
+        div.classList.add("list-item");
+        div.appendChild(checkbox);
+        div.appendChild(label);
+        listElement.appendChild(div);
+    });
+}
+
+initializeList();
+
+listElement.addEventListener("click", (event) => {
+    if (event.target.classList == "list-item") {
+        const checkbox = event.target.querySelector("input[type='checkbox']");
+        checkbox.checked = !checkbox.checked;
+    }
 });
