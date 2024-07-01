f535231f5c83aec1aaf354c1a0b2aa68d7d5d9c2
Marco Tuzza <marcotuzza@hotmail.it>
2024-07-01 22:58:32 +0400

    Improved visibility and list of items

--- app.js
+++ app.js
@@ -1,8 +1,106 @@
 // Define the list of items
 const list = {
-    "Fruit": ["apple", "banana", "cherry"],
-    "Snacks": ["chips", "chocolate"],
-    "Necessities": ["toothpaste", "soap"]
+    "Fruit": 
+    [
+        "apples", 
+        "banana", 
+        "cherry",
+        "apricot",
+        "grapefruit",
+        "grapes",
+        "kiwi",
+        "lemon",
+        "lime",
+        "mango",
+        "melon",
+        "nectarine",
+        "orange",
+        "peach"
+    ],
+    "Vegetables":
+    [
+        "potatoes",
+        "carrots",
+        "onions",
+        "lettuce",
+        "cucumber",
+        "tomatoes"
+    ],
+    "Dairy":
+    [
+        "milk",
+        "cheese",
+        "yogurt",
+        "butter",
+        "eggs",
+        "ice cream"
+    ],
+    "Meat":
+    [
+        "chicken",
+        "beef",
+        "pork",
+        "sausages",
+        "bacon",
+        "ground beef",
+        "salame",
+        "filet",
+        "ham",
+        "steak",
+        "chicken nuggets",
+        "chicken popcorn"
+    ],
+    "Snacks": 
+    [
+        "chips", 
+        "chocolate",
+        "breakfast cookies",
+        "crackers",
+        "popcorn",
+        "beef jerky",
+        "gum"
+    ],
+    "Miscellaneous": 
+    [
+        "toilet paper", 
+        "toothpaste", 
+        "soap",
+        "shampoo",
+        "body wash",
+        "deodorant",
+        "floss",
+        "pain reliever",
+        "cold medicine",
+        "tissues",
+        "ziploc bags",
+        "aluminum foil",
+        "plastic wrap",
+        "parchment paper",
+        "batteries",
+        "light bulbs",
+        "bug repellent",
+        "sunscreen",
+        "lotion"
+    ],
+    "Pastries":
+    [
+        "bread",
+        "lavash",
+        "donuts",
+        "hotdog",
+        "bakery pizza"
+    ],
+    "Grocery":
+    [
+        "rice",
+        "pasta",
+        "olive oil",
+        "sunflower oil",
+        "salt",
+        "sugar",
+        "pepper",
+        "spice set"
+    ]
 };
 
 // Get references to DOM elements
