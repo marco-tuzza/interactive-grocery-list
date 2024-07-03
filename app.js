98f20f7052db255c151fa243b5f0942aeeec0fae
Marco Tuzza <marcotuzza@hotmail.it>
2024-07-03 10:10:27 +0400

    Added dynamic visibility for copy list button

--- app.js
+++ app.js
@@ -1,107 +1,4 @@
-// Define the list of items
-const list = {
-    "Fruit": 
-    [
-        "apples", 
-        "banana", 
-        "cherry",
-        "apricot",
-        "grapefruit",
-        "grapes",
-        "kiwi",
-        "lemon",
-        "lime",
-        "mango",
-        "melon",
-        "nectarine",
-        "orange",
-        "peach"
-    ],
-    "Vegetables":
-    [
-        "potatoes",
-        "carrots",
-        "onions",
-        "lettuce",
-        "cucumber",
-        "tomatoes"
-    ],
-    "Dairy":
-    [
-        "milk",
-        "cheese",
-        "yogurt",
-        "butter",
-        "eggs",
-        "ice cream"
-    ],
-    "Meat":
-    [
-        "chicken",
-        "beef",
-        "pork",
-        "sausages",
-        "bacon",
-        "ground beef",
-        "salame",
-        "filet",
-        "ham",
-        "steak",
-        "chicken nuggets",
-        "chicken popcorn"
-    ],
-    "Snacks": 
-    [
-        "chips", 
-        "chocolate",
-        "breakfast cookies",
-        "crackers",
-        "popcorn",
-        "beef jerky",
-        "gum"
-    ],
-    "Miscellaneous": 
-    [
-        "toilet paper", 
-        "toothpaste", 
-        "soap",
-        "shampoo",
-        "body wash",
-        "deodorant",
-        "floss",
-        "pain reliever",
-        "cold medicine",
-        "tissues",
-        "ziploc bags",
-        "aluminum foil",
-        "plastic wrap",
-        "parchment paper",
-        "batteries",
-        "light bulbs",
-        "bug repellent",
-        "sunscreen",
-        "lotion"
-    ],
-    "Pastries":
-    [
-        "bread",
-        "lavash",
-        "donuts",
-        "hotdog",
-        "bakery pizza"
-    ],
-    "Grocery":
-    [
-        "rice",
-        "pasta",
-        "olive oil",
-        "sunflower oil",
-        "salt",
-        "sugar",
-        "pepper",
-        "spice set"
-    ]
-};
+// Products list in list.js file
 
 // Get references to DOM elements
 const listElement = document.getElementById("list");
@@ -135,6 +32,7 @@ function initializeList() {
 function clearList() {
     listElement.innerHTML = '';
     displayArea.innerHTML = '';
+    copyListButtonVisibility(false);
     initializeList();
 }
 
@@ -187,6 +85,7 @@ function setupGenerateListButtonListener() {
             listItem.textContent = checkbox.value.charAt(0).toUpperCase() + checkbox.value.slice(1);
             displayArea.appendChild(listItem);
         });
+        copyListButtonVisibility(true);
     });
 }
 
@@ -199,12 +98,17 @@ function setupCopyListButtonListener() {
     });
 }
 
+function copyListButtonVisibility(showButton = false) {
+    showButton === true ? copyListButton.style.display = 'block' : copyListButton.style.display = 'none';
+}
+
 // Initialize the list and setup event listeners
 function setupPage() {
     initializeList();
     setupListItemClickListener();
     setupGenerateListButtonListener();
     setupCopyListButtonListener();
+    copyListButtonVisibility();
 }
 
 // Call setupPage to initialize everything
